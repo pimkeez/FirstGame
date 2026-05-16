80baa1e013caeab239d4d85a981fb04ae85bd849
pimkeez <pinkiepieftw19@gmail.com>
2026-05-16 18:01:18 -0400

    camera-0 (#1)
    * implemented camera
    
    * made bool

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float boundX = 0.15f;
+    private Transform playerTransform;
+    private bool facingRight;
+   
+    void Awake()
+    {
+        playerTransform = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        float deltaX = playerTransform.position.x - transform.position.x;
+        if (deltaX > boundX || deltaX < -boundX)
+        {
+            if (transform.position.x < playerTransform.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        transform.position += new Vector3(delta.x, 0, 0);
+    }
+}
